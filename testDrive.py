69f661edb38e1f370652b1f01e7d89c2e43f2f6b
Snowding Zerland <181250011@smail.nju.edu.cn>
2020-07-29 13:03:00 +0800

    dev: 增加测试

--- testDrive.py
+++ testDrive.py
@@ -1,21 +1,19 @@
-import sys
-import json
-import codeLearn
 
+
+def testRank(num):
+	with open('rankTest', 'w') as f:
+		f.write("n\n")
+		f.write("snow\n")
+		for i in range(num):
+			f.write("T\n")
+			f.write("\n")
+			# if i % 5 == 0:
+			# 	print("50 0 7200000 6")
+			# else:
+			# 	print("100 0 300000 4")
+			f.write("100 0 300000 4\n")
+		# f.write("D\n")
+		f.write("Q\n")
 
 if __name__ == '__main__':
-	sys.stdout = open('rankTest','w')
-	for i in range(1,9):
-		for j in range(10):
-			print("n")
-			print("snow")
-			for i in range(800):
-				print("T")
-				print("")
-				# if i % 5 == 0:
-				# 	print("50 0 7200000 6")
-				# else:
-				# 	print("100 0 300000 4")
-				print("100 0 300000 4")
-			print("D")
-			print("Q")
+	testRank(100)
